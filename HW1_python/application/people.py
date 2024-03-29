4ca7be97184114c84f236f7669176b6d8bf5f139
Natalia Gromova <4gromov@gmail.com>
2024-03-29 11:01:21 +0300

    HW 1 python packages

---
+++ HW1_python/application/people.py
@@ -0,0 +1,4 @@
+def get_employees(list_employees):
+    name=input('Введите фамилию: ')
+    list_employees.append(name)
+    print(list_employees)
---
+++ HW1_python/main.py
@@ -0,0 +1,8 @@
+from application.salary import calculate_salary
+from application.people import get_employees
+from data import employees, d_fee
+import datetime
+if __name__ == '__main__':
+    print(datetime.datetime.now())
+    get_employees(employees)
+    calculate_salary(d_fee)
