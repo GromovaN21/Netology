ad035f2bb5e0a09e94c159628ec3ae213aad638b
Natalia Gromova <4gromov@gmail.com>
2024-03-29 11:07:57 +0300

    Changed names in import

--- HW1_python/main.py
+++ HW1_python/main.py
@@ -1,6 +1,6 @@
 from application.salary import calculate_salary
 from application.people import get_employees
-from data import employees, d_fee
+from data.data_input import employees, d_fee
 import datetime
 if __name__ == '__main__':
     print(datetime.datetime.now())
